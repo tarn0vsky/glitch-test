f2e458f4c4b41c9035b653f0e8f825784ba826de
Arthur Tarnovsky <arthur@tarnovsky.com>
2024-03-18 17:49:29 +0300

    /nga added

--- server.py
+++ server.py
@@ -10,6 +10,13 @@ def index():
     return "Привет от приложения Flask"
 
 
+@app.route("/nga")
+def nga():
+    return ("<h1>Привет, нига!</h1>"
+            "<h2>Я за тобой наблюдаю!</h2>")
+
+
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
